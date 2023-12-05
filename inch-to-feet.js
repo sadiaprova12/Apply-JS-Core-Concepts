160eada305ac5de64c16bac1c17310b83ce6aea4
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-05 20:53:15 +0600

    added dadi feet

--- inch-to-feet.js
+++ inch-to-feet.js
@@ -5,3 +5,7 @@ console.log(myFeet);
 const dataInches = 144;
 const dadaFeet = dataInches / 12;
 console.log('data feet', dadaFeet);
+
+const dadiInches = 60;
+const dadiFeet = dadiInches /12;
+console.log('dadiFeet', dadiFeet);
