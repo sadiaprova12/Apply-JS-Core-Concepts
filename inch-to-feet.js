fecbb1ef339ba8757acf3c857b1f5c5585ba5aed
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-07 21:50:30 +0600

    added new function

--- inch-to-feet.js
+++ inch-to-feet.js
@@ -1,11 +1,20 @@
-const myInches = 12;
-const myFeet = myInches /12;
-console.log(myFeet);
+// const myInches = 12;
+// const myFeet = myInches /12;
+// console.log(myFeet);
 
-const dataInches = 144;
-const dadaFeet = dataInches / 12;
-console.log('data feet', dadaFeet);
+// const dadaInches = 144;
+// const dadaFeet = dataInches / 12;
+// console.log('data feet', dadaFeet);
 
-const dadiInches = 60;
-const dadiFeet = dadiInches /12;
-console.log('dadiFeet', dadiFeet);
+// const dadiInches = 60;
+// const dadiFeet = dadiInches /12;
+// console.log('dadiFeet', dadiFeet);
+
+function inchToFeet(inches){
+      const feet = inches / 12;
+      return feet;
+}
+
+const dadaInches = 144;
+const dadaFeet = inchToFeet(dadaInches);
+console.log(dadaFeet);
