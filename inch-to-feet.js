b0c3db52773ade93b81da9bd108d639240cae0cb
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-08 23:46:09 +0600

    added nana feet

--- inch-to-feet.js
+++ inch-to-feet.js
@@ -18,3 +18,7 @@ function inchToFeet(inches){
 const dadaInches = 144;
 const dadaFeet = inchToFeet(dadaInches);
 console.log(dadaFeet);
+
+const nanaInches = 168;
+const nanaFeet = inchToFeet(nanaInches);
+console.log('nana feet', nanaFeet);
