18aeb8d332f266c34b21c242c90ac0421cf81c76
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-09 00:05:33 +0600

    added remainder even number

--- isEven.js
+++ isEven.js
@@ -1,9 +1,18 @@
+/** even number */
 // console.log(4/2);
 // console.log(98/2);
 // console.log(144/2);
 // console.log(1286/2);
 
-console.log(7/2);
-console.log(83/2);
-console.log(145/2);
-console.log(1287/2);
+
+/** Remainder even number */
+console.log(4%2);
+console.log(98%2);
+console.log(144%2);
+console.log(1286%2);
+
+/** odd number */
+// console.log(7/2);
+// console.log(83/2);
+// console.log(145/2);
+// console.log(1287/2);
