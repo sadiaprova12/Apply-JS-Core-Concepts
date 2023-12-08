9fbaf95b015b9806f545563193cb3b3d826b5b9e
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-09 00:07:19 +0600

    added remainder odd number

--- isEven.js
+++ isEven.js
@@ -16,3 +16,9 @@ console.log(1286%2);
 // console.log(83/2);
 // console.log(145/2);
 // console.log(1287/2);
+
+/** Remainder odd number */
+console.log(7%2);
+console.log(83%2);
+console.log(145%2);
+console.log(1287%2);
