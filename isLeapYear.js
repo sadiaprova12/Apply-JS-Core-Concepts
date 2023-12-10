31f203dffc3b8822cf96914b6601b07eca8ddd56
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-10 15:39:52 +0600

    added true or false leap year

--- isLeapYear.js
+++ isLeapYear.js
@@ -3,12 +3,16 @@
 function isLeapYear(year){
     const remainder = year % 4;
     if(remainder === 0){
-        console.log('Year year is leap year', year);
+        // console.log('Year year is leap year', year);
+        return true;
     }
     else{
-        console.log('Your year is not a leap year', year);
+        // console.log('Your year is not a leap year', year);
+        return false;
     }
 }
 
-isLeapYear(1933);
-isLeapYear(1960);
+const notLeapYear = isLeapYear(1933);
+console.log(notLeapYear);
+const leapYear = isLeapYear(1960);
+console.log(leapYear);
