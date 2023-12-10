6631afc5f2831b7c52359fe7c05acdc19c842923
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-10 15:18:33 +0600

    added new function

--- isEven.js
+++ isEven.js
@@ -18,7 +18,15 @@ console.log(1286%2);
 // console.log(1287/2);
 
 /** Remainder odd number */
-console.log(7%2);
-console.log(83%2);
-console.log(145%2);
-console.log(1287%2);
+// console.log(7%2);
+// console.log(83%2);
+// console.log(145%2);
+// console.log(1287%2);
+
+function isEven(number){
+    const remainder = number % 2;
+    console.log(remainder);
+}
+
+// isEven(48);
+isEven(35);
