680092e8d3203d977e69cc077550d17d59bec28a
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-10 16:22:41 +0600

    added another comments

--- isLeapYear.js
+++ isLeapYear.js
@@ -1,4 +1,17 @@
 // What is Leap Year//
+// javascript lepar logic google search
+
+// function isLeapYear(year){
+//     const remainder = year % 4;
+//     if(remainder === 0){
+//         // console.log('Year year is leap year', year);
+//         return true;
+//     }
+//     else{
+//         // console.log('Your year is not a leap year', year);
+//         return false;
+//     }
+// }
 
 function isLeapYear(year){
     const remainder = year % 4;
