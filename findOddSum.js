bffed931ea7c13da1926d5f5eb98b10b78e483c7
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-10 16:37:06 +0600

    added index elements

--- findOddSum.js
+++ findOddSum.js
@@ -1,8 +1,19 @@
 // Write a function findOddSum() that will take the array [5,7,8,10,45,30] as the inpust parameter and will return the sum of the odd numbers.//
 
-function getSum(numbers){
-console.log(numbers);
+// function getSum(numbers){
+// console.log(numbers);
+// }
+
+// const myNumbers = [12, 65, 45, 78, 32, 45, 91];
+// getSum(myNumbers);
+
+function getSumOfAnArray(numbers){
+   for(let i = 0; i < numbers.length; i++){
+    const index = i;
+    const element = numbers[index]
+    console.log(index, element);
+   }
 }
 
 const myNumbers = [12, 65, 45, 78, 32, 45, 91];
-getSum(myNumbers);
+getSumOfAnArray(myNumbers);
