c244ee0266bf341a7ce4280c351750040e349824
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-10 16:28:28 +0600

    added find odd sum

---
+++ findOddSum.js
@@ -0,0 +1,8 @@
+// Write a function findOddSum() that will take the array [5,7,8,10,45,30] as the inpust parameter and will return the sum of the odd numbers.//
+
+function getSum(numbers){
+console.log(numbers);
+}
+
+const myNumbers = [12, 65, 45, 78, 32, 45, 91];
+getSum(myNumbers);
