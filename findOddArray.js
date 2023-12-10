9dbca7046e36845ce9828a2e53eba2803f35c010
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-10 16:58:01 +0600

    added odd numbers array

--- findOddArray.js
+++ findOddArray.js
@@ -1,14 +1,19 @@
 function getOddNumbersArray(numbers){
+    // declare array
+    const oddNumbers = [];
      for(let i = 0; i < numbers.length; i++){
         // console.log(i);
         const index = i;
         const element = numbers[index];
         if(element % 2 !== 0){
             console.log(index, element);
+            oddNumbers.push(element);
         }
         
      }
+     return oddNumbers;
 }
 
 const myNumbers = [12, 65, 45, 78, 32, 45, 91];
-getOddNumbersArray(myNumbers);
+const oddNumbers = getOddNumbersArray(myNumbers);
+console.log(oddNumbers);
