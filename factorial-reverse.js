f0491dccae4313d58b4d96ace415e1e739045439
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-11 20:13:24 +0600

    change results

--- factorial-reverse.js
+++ factorial-reverse.js
@@ -1,7 +1,12 @@
 function factorial(numbers){
+    let result = 1;
     for(let i = numbers; i >= 1; i--){
+        result = result * i;
         console.log(i);
     }
+    return result;
 }
 
-factorial(9);
+const numbers = 6;
+const fact = factorial(numbers);
+console.log('factorial of:' , numbers, fact);
