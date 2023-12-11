eef0c6ac20982f22764acbe6b6deef06094691f2
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-11 19:41:36 +0600

    added factorial cmt

--- multiplicationOfNumbers.js
+++ multiplicationOfNumbers.js
@@ -1,5 +1,8 @@
 // 1 * 2 * 3 * 4 * 5 * 6 * 7
-
+/** Factorial (1-4 porjontto multiplication bujhay)
+ * 1.  3! = 3*2*1
+ * 2.  4! = 4*3*2*1
+ */
 function multiplicationOfNumbers(number){
     let result = 1;
     for(let i = 1; i <= 7; i++){
