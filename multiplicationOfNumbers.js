4b61b03912d0272bb7d8f88fb1e05094b9efcea5
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-11 19:36:50 +0600

    added multiplication

---
+++ multiplicationOfNumbers.js
@@ -0,0 +1,12 @@
+// 1 * 2 * 3 * 4 * 5 * 6 * 7
+
+function multiplicationOfNumbers(number){
+    let result = 1;
+    for(let i = 1; i <= 7; i++){
+        result = result * i;
+        console.log(i, result);
+    }
+    return result;
+}
+
+const result = multiplicationOfNumbers(7);
