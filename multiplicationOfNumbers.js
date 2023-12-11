28306e3fd28e6d157a1dd939b68f40551dcfb1dd
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-11 19:43:08 +0600

    factorial formula

--- multiplicationOfNumbers.js
+++ multiplicationOfNumbers.js
@@ -2,6 +2,7 @@
 /** Factorial (1-4 porjontto multiplication bujhay)
  * 1.  3! = 3*2*1
  * 2.  4! = 4*3*2*1
+ *  Factorial formula: n! = n * (n-1)!
  */
 function multiplicationOfNumbers(number){
     let result = 1;
